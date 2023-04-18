fa9884e8d02962f07fa231b884477e33f85a081f
rxsweet <105535100+rxsweet@users.noreply.github.com>
2023-04-18 15:56:20 +0800

    Add files via upload

---
+++ test_clash_latency.py
@@ -0,0 +1,83 @@
+import gzip         #https://www.cnblogs.com/eliwang/p/14591861.html
+import os
+import shutil   #主要：拷贝文件https://blog.csdn.net/weixin_41261833/article/details/108050152
+import subprocess   #子进程管理 https://zhuanlan.zhihu.com/p/91342640    https://www.runoob.com/w3cnote/python3-subprocess.html
+from concurrent.futures import ThreadPoolExecutor   #线程池 https://zhuanlan.zhihu.com/p/65638744 https://www.jianshu.com/p/6d6e4f745c27
+from urllib.parse import quote  #https://blog.csdn.net/weixin_43788986/article/details/125572389
+
+import requests #python中requests库使用方法详解 https://zhuanlan.zhihu.com/p/137649301  https://www.runoob.com/python3/python-requests.html
+
+
+def download(url, file, unpack_gzip=False):
+    os.makedirs(os.path.normpath(os.path.dirname(file)), exist_ok=True)
+    #os.path.dirname(path)功能：去掉文件名，返回目录 ,此处clash_path='/usr/local/bin/clash'，返回'/usr/local/bin/'
+    #os.path.normpath(path)，规范路径path字符串 https://blog.csdn.net/jn10010537/article/details/122769205
+    #os.makedirs(path,mode) 用于递归创建目录，path -- 需要递归创建的目录，可以是相对或者绝对路径，mode -- 权限模式
+    with (
+        #python中requests库使用方法详解 https://zhuanlan.zhihu.com/p/137649301  https://www.runoob.com/python3/python-requests.html
+        requests.get(url, headers={'Accept-Encoding': 'gzip'}, stream=True) as resp,
+        open(file, 'wb') as _out
+    ):
+        _in = resp.raw
+        if unpack_gzip or resp.headers.get('Content-Encoding') == 'gzip':
+            _in = gzip.open(_in)    #解压文件https://www.cnblogs.com/eliwang/p/14591861.html
+        shutil.copyfileobj(_in, _out)   #拷贝文件https://zhuanlan.zhihu.com/p/213919757 和 https://www.cnblogs.com/xiangsikai/p/7787101.html
+
+
+def test_latency(name, timeout=2000):
+    try:
+        #urllib.parse.quote()   https://blog.csdn.net/weixin_43788986/article/details/125572389
+        #quote() 介绍2：https://blog.csdn.net/weixin_43411585/article/details/89067127
+        r = requests.get(f"http://127.0.0.1:9090/proxies/{quote(name, safe='')}/delay", params={
+            'url': 'https://i.ytimg.com/generate_204',
+            'timeout': timeout
+        }, timeout=timeout / 400).json()
+    except Exception as e:
+        r = {'message': str(e)}
+    return r
+
+
+def test_all_latency(   #latency：潜伏
+    config_url: str = None, #函数注释（定义函数时使用“：”及“ ->”符号） https://blog.csdn.net/finalkof1983/article/details/87943032 https://blog.csdn.net/hou09tian/article/details/124435432
+    config_path='/etc/clash/config.yaml',
+    config_cover=True,
+    clash_url='https://github.com/Dreamacro/clash/releases/download/v1.14.0/clash-linux-amd64-v1.14.0.gz',
+    clash_path='/usr/local/bin/clash',
+    clash_cover=False,
+    max_workers=32,
+    timeout=2000,
+) -> list[tuple[str, dict]]:# tuple和list的区别：https://zhuanlan.zhihu.com/p/92527888 https://www.liaoxuefeng.com/wiki/1016959663602400/1017092876846880
+    if clash_cover or not os.path.exists(clash_path):   #os.path.exists()就是判断括号里的文件是否存在，括号内的可以是文件路径
+        download(clash_url, clash_path, unpack_gzip=True)#下载clash
+    if config_url and (config_cover or not os.path.exists(config_path)):
+        download(config_url, config_path)#下载config.yaml（实际就是节点文件）
+    os.chmod(clash_path, 0o755)#os.chmod() 方法用于更改文件或目录的权限。
+    
+    with subprocess.Popen([clash_path, '-f', config_path, '--ext-ctl', ':9090'], stdout=subprocess.PIPE) as popen:
+    #subprocess子进程管理 https://zhuanlan.zhihu.com/p/91342640
+    #自己推荐看这个 https://www.runoob.com/w3cnote/python3-subprocess.html
+    #https://blog.csdn.net/weixin_45314192/article/details/123310026
+        while b':9090' not in popen.stdout.readline():#为了停止popen.stdout.readline()吗？
+            pass    #pass 语句不执行任何操作。语法上需要一个语句，但程序不实际执行任何动作时，可以使用该语句，或者是当站位语句
+        try:
+            proxies = requests.get('http://127.0.0.1:9090/proxies').json()['proxies']
+            for k in ('DIRECT', 'REJECT', 'GLOBAL'):
+                del proxies[k]
+            #线程池 https://zhuanlan.zhihu.com/p/65638744 https://www.jianshu.com/p/6d6e4f745c27
+            #threadpoolexecutor.map() https://www.cnblogs.com/rainbow-tan/p/17269543.html
+            with ThreadPoolExecutor(max_workers) as executor:
+                return sorted(
+                    zip(proxies, executor.map(lambda name: test_latency(name, timeout), proxies)),
+                    key=lambda x: (x[1].get('meanDelay') or float('inf'), x[1].get('delay') or float('inf'))
+                )
+                #sorted() 函数对所有可迭代的对象进行排序操作 https://blog.csdn.net/PY0312/article/details/88956795
+                #zip() 函数用于将可迭代的对象作为参数,
+                #map() 会根据提供的函数对指定序列做映射 https://blog.csdn.net/PY0312/article/details/88956795
+                #将lambda函数赋值给一个变量，通过这个变量间接调用该lambda函数 https://blog.csdn.net/PY0312/article/details/88956795
+        finally:    #无论try语句中是否抛出异常，finally中的语句一定会被执行https://blog.csdn.net/gyniu/article/details/80345160
+            popen.terminate()#Popen.terminate()停止子进程
+
+
+if __name__ == '__main__':
+    for item in test_all_latency('https://dd.al/trial-All', timeout=10000):
+        print(*item)    #*参数，**参数 https://zhuanlan.zhihu.com/p/89304906  https://blog.csdn.net/cadi2011/article/details/84871401
