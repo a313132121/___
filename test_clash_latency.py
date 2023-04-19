d509fe84e8405bb886e288d627dd02ae3395e1a8
rxsweet <105535100+rxsweet@users.noreply.github.com>
2023-04-19 13:39:16 +0800

    Update test_clash_latency.py

--- test_clash_latency.py
+++ test_clash_latency.py
@@ -7,6 +7,7 @@ from urllib.parse import quote  #https://blog.csdn.net/weixin_43788986/article/d
 
 import requests #python中requests库使用方法详解 https://zhuanlan.zhihu.com/p/137649301  https://www.runoob.com/python3/python-requests.html
 import yaml
+import json
 outfile = 'xxx.yaml'
 def push(list, outfile):
 
